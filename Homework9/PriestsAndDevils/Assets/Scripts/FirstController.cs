25471f7620e73b8186fd0d839b7be01985d07421
wuzht <770333760@qq.com>
2018-06-18 20:41:56 +0800

    hw9

---
+++ Homework9/PriestsAndDevils/Assets/Scripts/FirstController.cs
@@ -0,0 +1,173 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstController : MonoBehaviour, ISceneController, IUserAction {
+    public CCActionManager actionManager;
+    public bool someObjectHandling;
+
+    public CoastController fromCoast;
+	public CoastController toCoast;
+	public BoatController boat;
+	private ICharacterController[] characters;
+    readonly Vector3 riverPosition = new Vector3(0, 0.5F, 0);
+    UserUI userUI;
+
+    //for the boat
+    private Vector3 fromPosition = new Vector3(4, 1, 0);
+    private Vector3 toPosition = new Vector3(-4, 1, 0);
+
+    void Awake() {
+		SSDirector director = SSDirector.GetInstance();
+        director.SetFPS(60);
+		director.currentSceneController = this;
+		userUI = gameObject.AddComponent<UserUI>() as UserUI;
+		characters = new ICharacterController[6];
+		LoadResources();
+	}
+
+    void Start()
+    {
+        actionManager = gameObject.AddComponent<CCActionManager>();
+    }
+
+    public void LoadResources() {
+        someObjectHandling = false;
+
+		GameObject river = Instantiate(Resources.Load("Perfabs/River", typeof(GameObject)), riverPosition, Quaternion.identity, null) as GameObject;
+		river.name = "river";
+		fromCoast = new CoastController("from");
+		toCoast = new CoastController("to");
+		boat = new BoatController();
+		LoadCharacter();
+	}
+
+	private void LoadCharacter() {
+		for (int i = 0; i < 3; i++) {
+            ICharacterController character = new ICharacterController("priest");
+            character.SetName("priest" + i);
+            character.SetPosition(fromCoast.GetEmptyPosition());
+            character.GetOnCoast(fromCoast);
+			fromCoast.GetOnCoast(character);
+			characters[i] = character;
+		}
+
+		for (int i = 0; i < 3; i++) {
+            ICharacterController character = new ICharacterController("devil");
+            character.SetName("devil" + i);
+            character.SetPosition(fromCoast.GetEmptyPosition());
+            character.GetOnCoast(fromCoast);
+			fromCoast.GetOnCoast(character);
+			characters[i + 3] = character;
+		}
+	}
+
+	public void MoveBoat(GameObject clickedObject) {
+        if (boat.IsEmpty())
+        {
+            someObjectHandling = false;
+            return;
+        }
+        
+        if (boat.toOrFrom == -1)
+        {
+            actionManager.MoveAction(clickedObject, fromPosition);
+            boat.toOrFrom = 1;
+        }
+        else
+        {
+            actionManager.MoveAction(clickedObject, toPosition);
+            boat.toOrFrom = -1;
+        }    
+        //boat.Move();
+        userUI.status = CheckGameOver();
+	}
+
+	public void CharacterIsClicked(ICharacterController characterController, GameObject clickedObject) {
+		if (characterController.IsOnBoat()) {
+			CoastController whichCoast;
+			if (boat.Get_to_or_from() == -1) // to -1; from 1
+				whichCoast = toCoast;
+			else
+				whichCoast = fromCoast;
+			boat.GetOffBoat(characterController.GetName());
+            actionManager.MoveAction(clickedObject, whichCoast.GetEmptyPosition());
+            //characterController.MoveToPosition (whichCoast.GetEmptyPosition());
+            characterController.GetOnCoast(whichCoast);
+			whichCoast.GetOnCoast(characterController);
+		}
+        else {
+			CoastController whichCoast = characterController.GetCoastController();
+			if (boat.GetEmptyIndex() == -1)
+				return;
+			if (whichCoast.GetToOrFrom() != boat.Get_to_or_from())
+				return;
+			whichCoast.GetOffCoast(characterController.GetName());
+            actionManager.MoveAction(clickedObject, boat.GetEmptyPosition());
+            //characterController.MoveToPosition (boat.GetEmptyPosition());
+            characterController.GetOnBoat(boat);
+			boat.GetOnBoat(characterController);
+		}
+		userUI.status = CheckGameOver();
+	}
+
+	int CheckGameOver() {	// playing 0, lose 1, win 2
+		int fromPriest = 0;
+		int fromDevil = 0;
+		int toPriest = 0;
+		int toDevil = 0;
+
+		int[] fromCount = fromCoast.GetCharacterNum();
+		fromPriest += fromCount[0];
+		fromDevil += fromCount[1];
+
+		int[] toCount = toCoast.GetCharacterNum();
+		toPriest += toCount[0];
+		toDevil += toCount[1];
+
+		if (toPriest + toDevil == 6)
+			return 2;
+		int[] boatCount = boat.GetCharacterNum ();
+		if (boat.Get_to_or_from () == -1) {
+			toPriest += boatCount[0];
+			toDevil += boatCount[1];
+		}
+        else {
+			fromPriest += boatCount[0];
+			fromDevil += boatCount[1];
+		}
+		if (fromPriest < fromDevil && fromPriest > 0)
+			return 1;
+		if (toPriest < toDevil && toPriest > 0)
+			return 1;
+		return 0;
+	}
+
+	public void ReStart() {
+        if (boat.toOrFrom == -1)
+        {
+            actionManager.MoveAction(boat.boat, fromPosition);
+            boat.toOrFrom = 1;
+        }
+        boat.Reset();
+        fromCoast.Reset();
+        toCoast.Reset();
+        for (int i = 0; i < characters.Length; i++)
+			characters [i].Reset();
+    }
+
+    public void NextOnBoat()
+    {
+
+    }
+
+    public void NextOffBoat()
+    {
+
+    }
+
+    public bool GetNextStatus()
+    {
+        return false;
+    }
+}
